30d30f0e4ee213abb2e3fd34d411373b125cee4d
Nikita <nikitasochi001@gmail.com>
2025-06-25 13:06:30 +0300

    KS-1-5 correction task 1

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -11,8 +11,5 @@ fun main(){
     hours = (minutes / SECONDS_IN_MINUTE).toShort()
     minutes = (minutes - SECONDS_IN_MINUTE).toShort()
 
-    val formattedHours = "%02d".format(hours)
-    val formattedMinutes = "%02d".format(minutes)
-    val formattedSeconds = "%02d".format(secondsRemaining)
     println("%02d:%02d:%02d".format(hours, minutes, secondsRemaining))
 }
