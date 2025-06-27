9b2b33c8f25f84298affb024669267b40870a1e0
Nikita <nikitasochi001@gmail.com>
2025-06-27 15:23:59 +0300

    KS-2-1 recreate task 1

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,6 +1,12 @@
 package org.example.lesson2
 
 fun main(){
-    val arithmeticMean = (3 + 4 + 3 + 5) / 4.0
-    println(arithmeticMean)
+    val firstStudentScore: Int = 3
+    val secondStudentScore: Int = 4
+    val thirdStudentScore: Int = 3
+    val fourthStudentScore: Int = 5
+    val countOfStudents: Float = 4.0F
+
+    val arithmeticMean = (firstStudentScore + secondStudentScore + thirdStudentScore + fourthStudentScore) / countOfStudents
+    println("%.2f".format(arithmeticMean))
 }
