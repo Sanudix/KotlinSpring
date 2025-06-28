172b2225112fe3388fc92c2745b03914ded6a8c8
Nikita <nikitasochi001@gmail.com>
2025-06-28 21:53:45 +0300

    KS-3-2 create task 2

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson3
+
+fun main(){
+    val birthSurname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val firstAge = 20
+    val marriedSurname = "Сидорова"
+    val secondAge = 22
+
+    println("$birthSurname $name $patronymic, $firstAge лет.")
+    println("$marriedSurname $name $patronymic, $secondAge лет.")
+}
