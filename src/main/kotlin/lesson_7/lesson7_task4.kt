f336e26236ff14f2a64a98b2e418d17b572114d1
Nikita <nikitasochi001@gmail.com>
2025-07-05 16:59:57 +0300

    KS-7-4 recreate task 4

--- src/main/kotlin/lesson_7/lesson7_task4.kt
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_7
 
-fun main(){
+fun main() {
     print("Введите количество секунд, которые нужно засечь: ")
     val seconds = readln().toInt()
 
-    for (i in seconds downTo 1){
+    for (i in seconds downTo 1) {
         println("Осталось секунд: $i")
         Thread.sleep(1000)
     }
