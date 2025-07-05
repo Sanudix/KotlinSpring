dd2b6961afbda44fa561e170cb847fc9290df796
Nikita <nikitasochi001@gmail.com>
2025-07-05 16:51:46 +0300

    KS-7-1 recreate task 1

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_7
 
-fun main(){
+fun main() {
     val numbers = 0..9
     val arabicSymbols = 'a'..'z'
     var password: String = ""
 
-    for (i in 1..3){
+    for (i in 1..3) {
         password += arabicSymbols.random()
         password += numbers.random()
     }
