b8e61e742fe01a7b5a6608b9b118f20d2f301de5
Nikita <nikitasochi001@gmail.com>
2025-07-05 19:25:38 +0300

    KS-7-5 recreate task 5

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_7
+
 import kotlin.random.Random
 
-fun main(){
+fun main() {
     var passwordLength: Int
     do {
         print("Введите генерируемую длину пароля (не меньше 6): ")
         passwordLength = readln().toInt()
-    } while(passwordLength < 6)
+    } while (passwordLength < 6)
 
     var randomNumber: Int
     val numberRange = 0..9
@@ -14,7 +15,11 @@ fun main(){
     val lowercaseArabicSymbols = 'a'..'z'
     var password: String = ""
 
-    for (i in 1..passwordLength){
+    password += numberRange.random()
+    password += uppercaseArabicSymbols.random()
+    password += lowercaseArabicSymbols.random()
+
+    for (i in 1..passwordLength - 3) {
         randomNumber = Random.nextInt(1, 4)
         when (randomNumber) {
             1 -> password += numberRange.random()
@@ -22,5 +27,6 @@ fun main(){
             3 -> password += lowercaseArabicSymbols.random()
         }
     }
-    println("Ваш пароль: $password")
+    val shuffledPassword = password.toList().shuffled().joinToString("")
+    println("Ваш пароль: $shuffledPassword")
 }
