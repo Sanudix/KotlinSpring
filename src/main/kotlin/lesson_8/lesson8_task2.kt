6eeac21d09e6646b9a7756b1f83b302230fde526
Nikita <nikitasochi001@gmail.com>
2025-07-05 20:21:21 +0300

    KS-8-2 recreate task 2

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_8
 
-fun main(){
-    val recipeOfChicken = arrayOf("курица", "молоко", "яйца", "сыр", "масло", "соль", "черный перец", "панировочные сухари")
+fun main() {
+    val recipeOfChicken =
+        arrayOf("курица", "молоко", "яйца", "сыр", "масло", "соль", "черный перец", "панировочные сухари")
     print("Введите название интересующего ингредиента: ")
     val nameOfIngredient = readln()
     var flag: Boolean = false
 
-    for(i in recipeOfChicken) {
-        if(nameOfIngredient == i){
+    for (i in recipeOfChicken) {
+        if (nameOfIngredient == i) {
             println("Ингредиент $nameOfIngredient в рецепте есть.")
             return
         }
