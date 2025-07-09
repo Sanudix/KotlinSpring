d812f742c5181ba1dd8ac9285c54efbae9f84b3d
Nikita <nikitasochi001@gmail.com>
2025-07-09 13:04:56 +0300

    KS-10-2 recreate task 2

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -6,10 +6,10 @@ fun main() {
     println("Введите логин и пароль:")
     val userName = readln()
     val userPassword = readln()
-    inputDataProcessing(inputName = userName, inputPassword = userPassword)
+    analyzData(inputName = userName, inputPassword = userPassword)
 }
 
-fun inputDataProcessing(inputName: String, inputPassword: String) {
+fun analyzData(inputName: String, inputPassword: String) {
     if (inputPassword.length >= FOUR && inputName.length >= FOUR) println("Вход выполнен.")
     else println("Логин или пароль недостаточно длинные.")
 }
