1adbb1c2ad4a6a85098dd5d1e4abef27a50e6bbb
Nikita <nikitasochi001@gmail.com>
2025-07-10 15:25:55 +0300

    KS-10-5 second recreate task 5

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -11,34 +11,36 @@ fun main() {
     print("Введите пароль: ")
     val userPassword = readLine()
 
-    val accessToken = generateToken(login = userLogin, password = userPassword)
-    if (accessToken == null)
-        println("Неудачная авторизация.")
-    else {
-        val listOfPurchases = getBasket(token = accessToken)
-        println("Список покупок: $listOfPurchases.")
-    }
+    authorization(login = userLogin, password = userPassword)
 }
 
 fun generateToken(login: String?, password: String?): String? {
-    if (LOGIN == login && PASSWORD == password) {
-        val numberRange = 0..9
-        val charRange = 'a'..'z'
-        var token: String = ""
-        token += numberRange.random()
-        token += charRange.random()
-        for (i in 0 until 30) {
-            val randomNumber = Random.nextInt(0, 2)
-            if (randomNumber % 2 == 0)
-                token += numberRange.random()
-            else
-                token += charRange.random()
-        }
+    val numberRange = 0..9
+    val charRange = 'a'..'z'
+    var token: String = ""
+    token += numberRange.random()
+    token += charRange.random()
+    for (i in 0 until 30) {
+        val randomNumber = Random.nextInt(0, 2)
+        if (randomNumber % 2 == 0)
+            token += numberRange.random()
+        else
+            token += charRange.random()
+    }
 
-        val shuffledToken = token.toMutableList().apply { shuffle() }.joinToString("")
-        return shuffledToken
-    } else {
-        return null
+    val shuffledToken = token.toMutableList().apply { shuffle() }.joinToString("")
+    return shuffledToken
+}
+
+fun authorization(login: String?, password: String?) {
+    if (LOGIN == login && PASSWORD == password) {
+        println("Авторизация прошла успешно.")
+        val accessToken = generateToken(login = login, password = password)
+        val listOfPurchases = getBasket(token = accessToken)
+        println("Список покупок: $listOfPurchases.")
+    }
+    else {
+        println("Авторизация провалена.")
     }
 }
 
