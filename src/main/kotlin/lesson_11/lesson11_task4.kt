c651a79e574f0251aac120b27cd8d7632be279cb
Nikita <nikitasochi001@gmail.com>
2025-07-10 17:00:39 +0300

    KS-11-3 recreate task 4

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -3,16 +3,14 @@ package org.example.lesson_11
 class RecipeCategory(
     val title: String,
     val titleImage: String,
-    val listOfRecipes: MutableList<String>,
-    val listOfRecipesImages: MutableList<String>,
+    val listOfRecipes: MutableList<Recipe>,
 ){}
 
 class Recipe(
     val title: String,
-    val titleImage: String,
+    val image: String,
     var numberOfPortions: Int,
-    val listOfIngredientsTitle: MutableList<String>,
-    val listOfIngredientQuantity: MutableList<String>,
+    val listOfIngredients: MutableList<Ingredient>,
     val cookingMethod: String,
     ){}
 
