fb71c64f11b2d124b77da0a7440199dac5082a84
Nikita <nikitasochi001@gmail.com>
2025-07-19 21:14:02 +0300

    KS-13-1 recreate task 1

--- src/main/kotlin/lesson_13/lesson13_task1.kt
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -3,11 +3,11 @@ package org.example.lesson_13
 class PhoneBook(
     val name: String,
     val number: Long,
-    var company: String? = null,
-    ){
+    var company: String?,
+) {
 }
 
 fun main() {
-    val book1 = PhoneBook("Альберт", 9881600124)
+    val book1 = PhoneBook("Альберт", 9881600124, null)
     val book2 = PhoneBook("Горик", 9110974172, "Мегафон")
 }
