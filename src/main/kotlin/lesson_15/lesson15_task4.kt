00fe149c4bd52f63cb0a2707ac7ed4a96ca1624d
Nikita <nikitasochi001@gmail.com>
2025-07-22 12:49:33 +0300

    KS-15-4 recreate task 4

--- src/main/kotlin/lesson_15/lesson15_task4.kt
+++ src/main/kotlin/lesson_15/lesson15_task4.kt
@@ -1,32 +1,40 @@
 package org.example.lesson_15
 
 interface Search {
-    fun searchProcess(element: Component, instrument: Instrument) {
-        println("Выполняется поиск ${element.name} для ${instrument.name}.")
+    fun searchComponent(element: Component, instrument: Instrument) {
+        println("Выполняется поиск ${element.name} ${element.model} для ${instrument.name} ${instrument.model}.")
+    }
+
+    fun searchInstrument(instrument: Instrument) {
+        println("Выполняется поиск ${instrument.name} ${instrument.model} в каталоге магазина.")
     }
 }
 
 abstract class MusicalSubject(
     val name: String,
+    val model: String,
     val count: Int,
 ) : Search
 
 class Instrument(
     name: String,
+    model: String,
     count: Int,
     val producingCountry: String,
     val releaseYear: Int,
-) : MusicalSubject(name, count)
+) : MusicalSubject(name, model, count)
 
 class Component(
     name: String,
+    model: String,
     count: Int,
     val instrumentName: String,
-) : MusicalSubject(name, count)
+) : MusicalSubject(name, model, count)
 
 fun main() {
-    val instrument = Instrument("Гитара", 27, "Япония", 2019)
-    val component = Component("Ремень", 110, "Гитара")
+    val instrument = Instrument("Гитара", "Yamaha F310", 27, "Япония", 2019)
+    val component = Component("Ремень", "SP141 BLU",110, "Гитара")
 
-    component.searchProcess( component, instrument)
+    component.searchComponent( component, instrument)
+    component.searchInstrument( instrument)
 }
