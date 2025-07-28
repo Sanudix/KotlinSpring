c9673434086b510d7bc6d4ff6cd2b35704a33d6a
Nikita <nikitasochi001@gmail.com>
2025-07-28 12:13:22 +0300

    KS-17-5 create task 5

---
+++ src/main/kotlin/lesson_17/lesson17_task5.kt
@@ -0,0 +1,43 @@
+package org.example.lesson_17
+
+class User(
+    private val realPassword: String,
+    private var realLogin: String,
+) {
+    var password: String
+        get() {
+            println("Текущий пароль: \"${printPassword(realPassword.length)}\".")
+            return printPassword(realPassword.length)
+        }
+        set(value) {
+            println("Вы не можете изменить пароль.")
+        }
+
+    var login: String
+        get() {
+            println("Текущую логин: \"$realLogin\".")
+            return realLogin
+        }
+        set(value) {
+            println("Логин изменен с \"${realLogin}\" на \"$value\".")
+            realLogin = value
+        }
+}
+
+fun printPassword(count: Int): String {
+    var temp: String = ""
+    for (i in 0 until count) {
+        temp += "*"
+    }
+    return temp
+}
+
+fun main() {
+    val user1 = User("123333", "Olya")
+
+    user1.password
+    user1.password = "gAf12"
+
+    user1.login
+    user1.login = "Nastia"
+}
