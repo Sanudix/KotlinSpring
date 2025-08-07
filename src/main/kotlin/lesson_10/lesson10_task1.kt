5c269d88ff0a9a17949b9fa358e6154a50b8d9d7
Nikita <nikitasochi001@gmail.com>
2025-08-07 16:51:52 +0300

    Reassembly commit

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,18 +1,18 @@
-package org.example.lesson_10
-
-import kotlin.random.Random
-
-fun main() {
-    val userValueOfRoll = rollOfTheDice()
-    println("Ход игрока. Значение кубика: ${userValueOfRoll}")
-    val computerValueOfRoll = rollOfTheDice()
-    println("Ход комьютера. Значение кубика: ${computerValueOfRoll}")
-
-    when{
-        userValueOfRoll > computerValueOfRoll -> println("Победило человечество.")
-        userValueOfRoll < computerValueOfRoll -> println("Победил Скайнет.")
-        userValueOfRoll == computerValueOfRoll -> println("Победила дружба.")
-    }
-}
-
-fun rollOfTheDice(): Int = Random.nextInt(1, 7)
+//package org.example.lesson_10
+//
+//import kotlin.random.Random
+//
+//fun main() {
+//    val userValueOfRoll = rollOfTheDice()
+//    println("Ход игрока. Значение кубика: ${userValueOfRoll}")
+//    val computerValueOfRoll = rollOfTheDice()
+//    println("Ход комьютера. Значение кубика: ${computerValueOfRoll}")
+//
+//    when{
+//        userValueOfRoll > computerValueOfRoll -> println("Победило человечество.")
+//        userValueOfRoll < computerValueOfRoll -> println("Победил Скайнет.")
+//        userValueOfRoll == computerValueOfRoll -> println("Победила дружба.")
+//    }
+//}
+//
+//fun rollOfTheDice(): Int = Random.nextInt(1, 7)
