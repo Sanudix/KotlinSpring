93c081de3983ba9b03033dd4ec2e4955a53f2850
Nikita <nikitasochi001@gmail.com>
2025-08-07 17:19:22 +0300

    KS-20-3 create task 3

---
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_20
+
+class Gamer(val keyAvailable: Boolean = false,){
+    fun getStatus() = keyAvailable
+}
+
+fun main() {
+    val gamer1 = Gamer( false)
+    val gamer2 = Gamer(true)
+
+    val checkKey: (Gamer) -> String = { gamer: Gamer ->
+        if (gamer.getStatus()) "Игрок открыл дверь."
+        else "Дверь заперта."
+    }
+
+    val checkResult1 = checkKey(gamer1)
+    val checkResult2 = checkKey(gamer2)
+
+    println(checkResult1)
+    println(checkResult2)
+}
