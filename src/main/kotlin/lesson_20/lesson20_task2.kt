14ca896dc1add030fc881c346880840c36c79064
Nikita <nikitasochi001@gmail.com>
2025-08-07 11:56:15 +0300

    KS-20-2 create task 2

---
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_20
+
+class Player(
+    private val name: String,
+    private var currentHealth: Int,
+    private val maxHealth: Int,
+) {
+    fun printHealth() {
+        println("Здоровье игрока: $currentHealth/$maxHealth.")
+    }
+
+    fun getMaxHealth() = maxHealth
+    fun setCurrentHealth() {
+        currentHealth = maxHealth
+    }
+}
+
+fun main() {
+    val player1 = Player("Geralt", 64, 100)
+
+    val heal: (Player) -> Unit = { player: Player ->
+        player.setCurrentHealth()
+    }
+
+    player1.printHealth()
+}
