b4c4b35d68f72b302888ac5af95c6fce2972c2df
Nikita <nikitasochi001@gmail.com>
2025-08-10 10:31:15 +0300

    KS-20-4 create task 4

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -3,7 +3,7 @@ package org.example.lesson_20
 fun main() {
     val list = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
 
-    var newList = list.filterIndexed { index, _ -> index % 2 == 0 }.map { { println("Нажат элемент $it") } }
+    val newList = list.filterIndexed { index, _ -> index % 2 == 0 }.map { { println("Нажат элемент $it") } }
 
     println(list)
     newList.forEach {
