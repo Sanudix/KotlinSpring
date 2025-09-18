8e68533d791df03798aeb500ab32614e63d476ce
nik.bolotnikov <nikitasochi001@gmail.com>
2025-09-18 21:38:11 +0300

    KS-21-2 recreate task 2

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -4,18 +4,9 @@ fun main() {
     val numberList = listOf(1, 2, 3, 10)
 
     println("Список чисел: $numberList")
-    println("Сумма четных чисел списка: ${numberList.evenNumbersSum(numberList)}")
+    println("Сумма четных чисел списка: ${numberList.evenNumbersSum()}")
 }
 
-fun List<Int>.evenNumbersSum(_numberList: List<Int>): Int {
-    var sumEvenNumber: Int = 0
-    val evenNumberList = mutableListOf<Int>()
-
-    for (number in _numberList) {
-        if (number % 2 == 0) {
-            sumEvenNumber += number
-        }
-    }
-
-    return sumEvenNumber
+fun List<Int>.evenNumbersSum(): Int {
+    return this.filter { it % 2 == 0 }.sum()
 }
