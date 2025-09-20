4507c71bd495bdf930652ecd18016c98d4e260e9
nik.bolotnikov <nikitasochi001@gmail.com>
2025-09-20 12:52:31 +0300

    KS-21-5 create task 5

---
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_21
+
+fun main() {
+    val playerMap = mapOf<String, Int>(
+        "Скрытность" to 2000,
+        "Выносливость" to 2300,
+        "Интеллект" to 4000,
+        "Ловкость" to 3250,
+        "Харизма" to 4000
+    )
+
+    println(playerMap.maxCategory())
+}
+
+fun Map<String, Int>.maxCategory(): String {
+    val newMap = this.maxBy { it.value }
+    return newMap.key
+}
