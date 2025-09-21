69a78390aa565afff12213e15f0d9dbc3661e9a0
nik.bolotnikov <nikitasochi001@gmail.com>
2025-09-21 21:40:31 +0300

    KS-22-3 recreate task 3

--- src/main/kotlin/lesson_23/lesson23_task3.kt
+++ src/main/kotlin/lesson_23/lesson23_task3.kt
@@ -3,9 +3,7 @@ package org.example.lesson_23
 fun main() {
     val human1 = Human(Gender.MALE, 24, "Russia")
 
-    val humanGender = human1.gender
-    val humanAge = human1.age
-    val humanCountry = human1.country
+    val (humanGender, humanAge, humanCountry) = human1
 
     println(
         """
